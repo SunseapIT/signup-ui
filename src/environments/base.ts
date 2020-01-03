49946cc1fb254445a9ae7ef170d86135436cf947
Deepa <deepa.maywad@thrymr.net>
2020-01-03 19:42:20 +0530

    Deployment-->fixed table alignment issue

--- src/environments/base.ts
+++ src/environments/base.ts
@@ -5,7 +5,8 @@ export const environment = {
   subDomain: false,
   apiUrl: '',
   reCaptchaSiteKey: '',
-  // baseUrl: "http://18.139.224.201:8085"
-   baseUrl :"http://111.93.31.228:9091"
+  baseUrl: "http://18.139.224.201:8085"
+  //  baseUrl :"http://111.93.31.228:9091"
+  // baseUrl: "http://2caf3f44.ngrok.io"
  
 }

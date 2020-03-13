c2e4362554a80fc4121b28a1b5a3fdc3f390837b
Deepa Maywad <deepa.maywad@thrymr.net>
2020-03-13 15:41:52 +0530

    Fixed promocode error message issue

--- src/app/sign-up/admin/promo-code/add-promocode/add-promocode.component.ts
+++ src/app/sign-up/admin/promo-code/add-promocode/add-promocode.component.ts
@@ -11,74 +11,81 @@ import * as moment from 'moment'
   styleUrls: ['./add-promocode.component.scss']
 })
 export class AddPromocodeComponent implements OnInit {
-  model:any ={ promoCode : '', dateFrom : '', dateTo : '', infinity : "true", noOfLimitedUsers : ''  };
-  public dateTimeRange:any;
-  isLoader:boolean;
-  responseData:any={}
+  model: any = { promoCode: '', dateFrom: '', dateTo: '', infinity: "true", noOfLimitedUsers: '' };
+  public dateTimeRange: any[];
+  isLoader: boolean;
+  responseData: any = {}
   min = new Date();
-  hour12Timer:boolean= false
+  hour12Timer: boolean = false
 
-  constructor(private service : ApiServiceServiceService,
-    private dateFormat:DatePipe,
+  constructor(private service: ApiServiceServiceService,
+    private dateFormat: DatePipe,
     private router: Router,
-    private activatedRoute : ActivatedRoute,
-    private toastr:ToastrService) { }
+    private activatedRoute: ActivatedRoute,
+    private toastr: ToastrService) { }
 
 
 
   ngOnInit() {
+    this.responseData.infinity = "true";
     this.getPromoCodeById();
   }
 
 
-  getPromoCodeById(){
+  getPromoCodeById() {
     let id = this.activatedRoute.snapshot.params['id'];
-   if(id!=null){
-   this.service.get_service(ApiServiceServiceService.apiList.getPromoCodeById+"?promoId="+id).subscribe((response:any)=>{
-    var responseBody = response['body'];
-    var responseData = responseBody['data'];
-    var responseContent = responseData['content'];
-     let datefrom:any[]=[]
-     datefrom.push(moment(responseData.dateFrom,"DD-MM-YYYY,H:mm:ss").format())
-     datefrom.push(moment(responseData.dateTo,"DD-MM-YYYY,H:mm:ss").format())
-     this.responseData = responseData
-     this.dateTimeRange = datefrom;
-   })
-  }
-  }
-
-
-getTimeStamp(time){
-  return this.dateFormat.transform(time,"dd-MM-yyyy H:mm:ss");
-}
-
-  onSubmit(form: NgForm){
-    if(form.valid){
-      this.isLoader=true;
-    this.responseData.dateFrom=this.getTimeStamp(this.dateTimeRange[0])
-    this.responseData.dateTo=this.getTimeStamp(this.dateTimeRange[1])
-    this.responseData.noOfLimitedUsers=this.responseData.infinity?null:this.responseData.noOfLimitedUsers;
-    if(form.valid){
-      this.isLoader=true;
-      this.service.post_service(ApiServiceServiceService.apiList.addPromoCodeUrl,this.responseData).subscribe((response)=>{
+    if (id != null) {
+      this.service.get_service(ApiServiceServiceService.apiList.getPromoCodeById + "?promoId=" + id).subscribe((response: any) => {
         var responseBody = response['body'];
         var responseData = responseBody['data'];
-        let statusCode = responseBody['statusCode']
-        if(statusCode == 200){
-        this.isLoader=false;
-        this.router.navigateByUrl('/admin-login/admin-dash/view-promo')
-        this.toastr.success('', 'Promo Code added successfully', {
-         timeOut: 2000
-         });
-       }
+        var responseContent = responseData['content'];
+        let datefrom: any[] = []
+        datefrom.push(moment(responseData.dateFrom, "DD-MM-YYYY,H:mm:ss").format())
+        datefrom.push(moment(responseData.dateTo, "DD-MM-YYYY,H:mm:ss").format())
+        this.responseData = responseData;
+        this.responseData.infinity = this.responseData.infinity ? "true" : "false";
+        this.dateTimeRange = datefrom;
       })
-      form.resetForm();
+    }
   }
- }
- else{
-  this.toastr.error('', 'Please fill the form.', {
-    timeOut: 2000
-    });
- }
-}
+
+
+  getTimeStamp(time) {
+    return this.dateFormat.transform(time, "dd-MM-yyyy H:mm:ss");
+  }
+
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      this.responseData.dateFrom = this.getTimeStamp(this.dateTimeRange[0])
+      this.responseData.dateTo = this.getTimeStamp(this.dateTimeRange[1])
+      this.responseData.noOfLimitedUsers = this.responseData.infinity ? null : this.responseData.noOfLimitedUsers;
+      if (this.dateTimeRange[0] && this.dateTimeRange[1]) {
+        this.isLoader = true;
+        this.service.post_service(ApiServiceServiceService.apiList.addPromoCodeUrl, this.responseData).subscribe((response) => {
+          this.isLoader = true;
+          var responseBody = response['body'];
+          var responseData = responseBody['data'];
+          let statusCode = responseBody['statusCode'];
+          var responseMsg = responseBody['message'];
+          if (statusCode == 200) {
+            this.isLoader = false;
+            this.router.navigateByUrl('/admin-login/admin-dash/view-promo');
+            this.toastr.success('', 'Promo Code added successfully');
+            form.resetForm();
+          }
+          else {
+            this.toastr.error('', responseMsg);
+            this.isLoader = false;
+
+          }
+        })
+
+      } else {
+        this.toastr.error('', 'Please fill the form.');
+      }
+    }
+    else {
+      this.toastr.error('', 'Please fill the form.');
+    }
+  }
 }

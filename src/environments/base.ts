4b6652db6ceef4a130f786b9b0c6b5fc5e00fd0e
jsn koushik <koushik.j@thrymr.net>
2020-04-30 16:08:53 +0530

    signup issue

--- src/environments/base.ts
+++ src/environments/base.ts
@@ -6,7 +6,8 @@ export const environment = {
   apiUrl: '',
   reCaptchaSiteKey: '',
   // baseUrl: "api/",
-  baseUrl : "http://signup-uat.sunseap.com:8085",
+  // baseUrl : "http://signup-uat.sunseap.com:8085",
+  baseUrl : "http://ci.thrymr.net:9091",
   // baseUrl :"http://eb4f1023.ngrok.io",
   dialogUrl: '',
   // baseUrl: "http://111.93.31.228:9091"

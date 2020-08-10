474bc7e6f30f47397106964eebfe9656c10be431
Koushik Jsn <koushik.j@thrymr.net>
2020-08-10 19:58:58 +0530

    style changes

--- src/environments/base.ts
+++ src/environments/base.ts
@@ -5,10 +5,11 @@ export const environment = {
   subDomain: false,
   apiUrl: '',
   reCaptchaSiteKey: '',
-  // baseUrl: "api/",
-  baseUrl : "http://signup-uat.sunseap.com:8086",
+  baseUrl: "api/",
+  // baseUrl : "http://signup-uat.sunseap.com:8086",
   // baseUrl : "https://signup-uat.sunseap.com/api/",
   // baseUrl : "http://d568fe10d0b2.ngrok.io",
+  // baseUrl:"http://ci.thrymr.net:9093",
   dialogUrl: ''
 }
 
